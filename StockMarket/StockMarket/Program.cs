dacdb0ecae9a14d45d1a53abd0252837f863819d
Ancol <trandieu@gmail.com>
2024-10-06 22:01:31 +0700

    Config Program.Cs

--- StockMarket/StockMarket/Program.cs
+++ StockMarket/StockMarket/Program.cs
@@ -1,27 +1,19 @@
+using ServiceContracks;
+using Services;
+using StockMarket;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Services
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
+builder.Services.AddSingleton<IFinnhubService, FinnhubService>();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
--- StockMarket/StockMarket/TradingOptions.cs
+++ StockMarket/StockMarket/TradingOptions.cs
@@ -2,7 +2,7 @@
 /// <summary>
 /// Represents Options pattern for "StockPrice" configuration
 /// </summary>
-public class TradingOption
+public class TradingOptions
 {
     public string? DefaultStockSymbol {  get; set; }
 }
