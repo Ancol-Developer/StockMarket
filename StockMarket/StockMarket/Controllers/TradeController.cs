3eb3b1114c8419bfd2d7fe8bc6dd235dc29d9d71
Ancol <trandieu@gmail.com>
2024-10-06 22:02:04 +0700

    Add tradeController Add View Trade

---
+++ StockMarket/StockMarket/Controllers/TradeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StockMarket.Controllers
+{
+    public class TradeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
