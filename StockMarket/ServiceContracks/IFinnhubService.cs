f208048f9ecffc54af5dc4f09e488483623155ce
Ancol <trandieu@gmail.com>
2024-10-06 22:02:19 +0700

    Add FinnhubService

--- StockMarket/ServiceContracks/IFinnhubService.cs
+++ StockMarket/ServiceContracks/IFinnhubService.cs
@@ -1,6 +1,18 @@
 ﻿namespace ServiceContracks;
 
-public class IFinnhubService
+public interface IFinnhubService
 {
+    /// <summary>
+    /// Return company details such as company country, currency, exchange, IPO date, logo Image, ...
+    /// </summary>
+    /// <param name="stockSymbol">>Stock symbol to search</param>
+    /// <returns>Returns a dictionary that contains details such as company country, currency, exchange, IPO date, logo image, market capitalization, name of the company, phone number etc.</returns>
+    Dictionary<string, object>? GetCompanyProfile(string stockSymbol);
 
+    /// <summary>
+    /// Returns stock price details such as current price, change in price, percentage change, high price of the day, low price of the day, open price of the day, previous close price
+    /// </summary>
+    /// <param name="stockSymbol"></param>
+    /// <returns></returns>
+    Dictionary<string, object>? GetStockPriceQuote(string stockSymbol);
 }
--- StockMarket/Services/FinnhubService.cs
+++ StockMarket/Services/FinnhubService.cs
@@ -1,6 +1,78 @@
-﻿namespace Services;
+﻿using Microsoft.Extensions.Configuration;
+using ServiceContracks;
+using System.Net.Http;
+using System.Text.Json;
 
-public class FinnhubService
+namespace Services;
+
+public class FinnhubService : IFinnhubService
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IConfiguration _configuration;
+    public FinnhubService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    {
+        _httpClientFactory = httpClientFactory;
+        _configuration = configuration;
+    }
+    public Dictionary<string, object>? GetCompanyProfile(string stockSymbol)
+    {
+        //create http client
+        HttpClient httpClient = _httpClientFactory.CreateClient();
 
+        //create http request
+        HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri($"https://finnhub.io/api/v1/stock/profile2?symbol={stockSymbol}&token=cs0bit1r01qrbtrlar3gcs0bit1r01qrbtrlar40") //URI includes the secret token
+        };
+
+        // Send Request
+        HttpResponseMessage httpResponseMessage = httpClient.Send(httpRequestMessage);
+
+        //read response body
+        string responseBody = new StreamReader(httpResponseMessage.Content.ReadAsStream()).ReadToEnd();
+
+        //convert response body (from JSON into Dictionary)
+        Dictionary<string, object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseBody);
+
+        if (responseDictionary == null)
+            throw new InvalidOperationException("No response from server");
+
+        if (responseDictionary.ContainsKey("error"))
+            throw new InvalidOperationException(Convert.ToString(responseDictionary["error"]));
+
+        //return response dictionary back to the caller
+        return responseDictionary;
+    }
+
+    public Dictionary<string, object>? GetStockPriceQuote(string stockSymbol)
+    {
+        //create http client
+        HttpClient httpClient = _httpClientFactory.CreateClient();
+
+        //create http request
+        HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri($"https://finnhub.io/api/v1/quote?symbol={stockSymbol}&token=cs0bit1r01qrbtrlar3gcs0bit1r01qrbtrlar40") //URI includes the secret token
+        };
+
+        //send request
+        HttpResponseMessage httpResponseMessage = httpClient.Send(httpRequestMessage);
+
+        //read response body
+        string responseBody = new StreamReader(httpResponseMessage.Content.ReadAsStream()).ReadToEnd();
+
+        //convert response body (from JSON into Dictionary)
+        Dictionary<string, object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(responseBody);
+
+        if (responseDictionary == null)
+            throw new InvalidOperationException("No response from server");
+
+        if (responseDictionary.ContainsKey("error"))
+            throw new InvalidOperationException(Convert.ToString(responseDictionary["error"]));
+
+        //return response dictionary back to the caller
+        return responseDictionary;
+    }
 }
